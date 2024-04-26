a0ddf50b6ded613728d2a80a015b85ebca03327c
Will Haese-Hill <wahhill@gmail.com>
2024-04-26 13:13:00 +0100

    prot header should be uniprot acc for later matching

--- get_prot_seq_for_uniprot_acc.py
+++ get_prot_seq_for_uniprot_acc.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     if response.status == 200:
         reader = codecs.getreader("utf-8")
         obj = json.load(reader(response))
-        print(">" + obj[0]["uniprotId"])
+        print(">" + acc)
         print(obj[0]["uniprotSequence"])
