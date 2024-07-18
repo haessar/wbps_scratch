804055e628a093f62985b4b2eed539d9004c9cc0
Will Haese-Hill <wahhill@gmail.com>
2024-07-18 16:33:16 +0100

    Create run_busco.sh

---
+++ slurm_scripts/run_busco.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+############# SLURM SETTINGS #############
+#SBATCH --account=project0031   # account name (mandatory), if the job runs under a project then it'll be the project name, if not then it should =none
+#SBATCH --job-name=Xx_busco        # some descriptive job name of your choice
+#SBATCH --output=%x-%j.out      # output file name will contain job name + job ID
+#SBATCH --error=%x-%j.err       # error file name will contain job name + job ID
+#SBATCH --partition=nodes        # which partition to use, default on MARS is “nodes"
+#SBATCH --time=0-01:00:00       # time limit for the whole run, in the form of d-hh:mm:ss, also accepts mm, mm:ss, hh:mm:ss, d-hh, d-hh:mm
+#SBATCH --mem=128G                # memory required per node, in the form of [num][M|G|T]
+#SBATCH --nodes=1               # number of nodes to allocate, default is 1
+#SBATCH --ntasks=1              # number of Slurm tasks to be launched, increase for multi-process runs ex. MPI
+#SBATCH --cpus-per-task=16       # number of processor cores to be assigned for each task, default is 1, increase for multi-threaded runs
+#SBATCH --ntasks-per-node=1     # number of tasks to be launched on each allocated node
+
+############# LOADING MODULES (optional) #############
+module load apps/busco
+module load apps/genometools
+
+############# MY CODE #############
+~/wbps_scratch/busco_preprocessing.sh SEQFILE ANNFILE > proteins.fasta
+busco -i proteins.fasta -m proteins -l [metazoa_odb10|nematoda_odb10] -c 16 -o busco
