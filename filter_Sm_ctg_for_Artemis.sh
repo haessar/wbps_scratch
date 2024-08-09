a1a6e9b601ab346eeda5e5a39f7e3519b3b2f565
Will Haese-Hill <wahhill@gmail.com>
2024-08-09 10:05:06 +0100

    expand artemis filtering scripts for Sman and Hcon

--- filter_Sm_ctg_for_Artemis.sh
+++ filter_Sm_ctg_for_Artemis.sh
@@ -4,6 +4,11 @@ ctg_num=$1
 samtools faidx schistosoma_mansoni.PRJEA36577.WBPS19.genomic.fa SM_V10_${ctg_num} > SM_V10_${ctg_num}.fa
 awk "/SM_V10_$ctg_num\_/" Schistosoma_mansoni_braker3_full.gff3 > SM_V10_${ctg_num}_braker.gff
 awk -v num="$ctg_num" '$1=="SM_V10_"num' Schistosoma_mansoni_helixer_full.gff3 > SM_V10_${ctg_num}_helixer.gff
+awk -v num="$ctg_num" '$1=="SM_V10_"num' schistosoma_mansoni_gca000000000v1.gff3 > SM_V10_${ctg_num}_anno.gff
 awk -v num="$ctg_num" '$1=="SM_V10_"num' schistosoma_mansoni.PRJEA36577.WBPS19.annotations.gff3 > SM_V10_${ctg_num}_wbps.gff
 
-~/Artemis/art -Dblack_belt_mode=false SM_V10_${ctg_num}.fa + SM_V10_${ctg_num}_wbps.gff + SM_V10_${ctg_num}_braker.gff + SM_V10_${ctg_num}_helixer.gff
+~/Artemis/art -Dblack_belt_mode=false SM_V10_${ctg_num}.fa \
+        + SM_V10_${ctg_num}_wbps.gff \
+        + SM_V10_${ctg_num}_braker.gff \
+        + SM_V10_${ctg_num}_helixer.gff \
+        + SM_V10_${ctg_num}_anno.gff
