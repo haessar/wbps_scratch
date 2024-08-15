4dac7f07a8798adbbf4dd155ddffdc788fcf86e6
Will Haese-Hill <wahhill@gmail.com>
2024-08-15 14:58:44 +0100

    fix fisher exact logic

--- reannotation/statistics.py
+++ reannotation/statistics.py
@@ -4,23 +4,24 @@ from scipy.stats import fisher_exact
 
 
 def fisher_exact_for_two_lists_of_accessions(l1, l2):
-    more_expressed = {}
-    less_expressed = {}
-    evenly_expressed = {}
-    for acc, freq2 in Counter(l2).items():
+    more_frequent = {}
+    less_frequent = {}
+    no_significant_difference = {}
+    not_occurring = []
+    for acc, freq1 in Counter(l1).items():
         try:
-            freq1 = dict(Counter(l1))[acc]
+            freq2 = dict(Counter(l2))[acc]
         except KeyError:
-            print(f"{acc} not present in any l1 gene")
+            not_occurring.append(acc)
             continue
         other1 = len(l1) - freq1
         other2 = len(l2) - freq2
         res = fisher_exact([[freq1, freq2], [other1, other2]])
         if res.pvalue < 0.05:
             if res.statistic > 1:
-                less_expressed[acc] = res.statistic
+                more_frequent[acc] = res.statistic
             elif res.statistic < 1:
-                more_expressed[acc] = res.statistic
+                less_frequent[acc] = res.statistic
         else:
-            evenly_expressed[acc] = res.statistic
-    return more_expressed, evenly_expressed, less_expressed
+            no_significant_difference[acc] = res.statistic
+    return more_frequent, no_significant_difference, less_frequent, not_occurring
