2b8a787211b9d224f6d096f99a3e82dc61795841
Will Haese-Hill <wahhill@gmail.com>
2024-08-20 23:48:09 +0100

    extract_accessions_from_tsv

--- reannotation/utils.py
+++ reannotation/utils.py
@@ -1,5 +1,9 @@
 import re
 
+import pandas as pd
+
+ACC_COL = 11
+DESC_COL = 12
 
 def extract_accessions_from_transcript(tran):
     try:
@@ -10,3 +14,9 @@ def extract_accessions_from_transcript(tran):
     print(info)
     for acc in info.split("\n"):
         yield re.search(r'IPR\d+', acc).group(), acc.split("description:")[1]
+
+
+def extract_accessions_from_tsv(tsv_file):
+    data = pd.read_csv(tsv_file, sep="\t")
+    for acc, desc in data[data.iloc[:, ACC_COL].str.startswith("IPR")].iloc[:, [ACC_COL, DESC_COL]].to_records(index=False):
+        yield acc, desc
