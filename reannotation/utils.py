7fe56aedaaeee0033721f2ba6d8f33209173c93c
Will Haese-Hill <wahhill@gmail.com>
2024-11-14 14:27:01 +0000

    allow API lookup of IPR data

--- reannotation/utils.py
+++ reannotation/utils.py
@@ -1,22 +1,46 @@
 import re
 
 import pandas as pd
+import requests
+
+from utils.generic import flatten_list_to_list
 
 ACC_COL = 11
 DESC_COL = 12
+INTERPRO_API = "https://www.ebi.ac.uk/interpro/api/entry/interpro/"
 
-def extract_accessions_from_transcript(tran):
+
+def extract_accessions_from_transcript(tran, lookup=False, acc_product=None):
     try:
         info = tran.attributes.get("info")[0]
     except TypeError:
-        print(f"No InterPro accessions found for transcript {tran.id}")
         return
-    print(info)
-    for acc in info.split("\n"):
-        yield re.search(r'IPR\d+', acc).group(), acc.split("description:")[1]
+    for attr in info.split("\n"):
+        acc = re.search(r'IPR\d+', attr).group()
+        desc = attr.split("description:")[1].strip()
+        if acc_product is not None and acc in flatten_list_to_list(acc_product.values()):
+            continue
+        if lookup:
+            resp = requests.get(INTERPRO_API + acc, timeout=30).json()
+            try:
+                yield acc, desc, resp["metadata"]["type"]
+            except KeyError:
+                pass
+        else:
+            yield acc, desc
 
 
-def extract_accessions_from_tsv(tsv_file):
+def extract_accessions_from_tsv(tsv_file, lookup=False, acc_product=None):
     data = pd.read_csv(tsv_file, sep="\t")
     for acc, desc in data[data.iloc[:, ACC_COL].str.startswith("IPR")].iloc[:, [ACC_COL, DESC_COL]].to_records(index=False):
-        yield acc, desc
+        if acc_product is not None and acc in flatten_list_to_list(acc_product.values()):
+            continue
+        if lookup:
+            resp = requests.get(INTERPRO_API + acc, timeout=30).json()
+            try:
+                yield acc, desc, resp["metadata"]["type"]
+            except KeyError:
+                pass
+        else:
+            yield acc, desc
+
